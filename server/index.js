9a8460193e8a9278755cebf81524c04dddc42abe
JeffOrdze <jeff.ordze@live.ca>
2023-06-02 09:05:11 -0600

    initial server setup

--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ const catsRoute = require("./routes/cats")
 
 app.use(cors())
 app.use(express.json())
-app.use('/static, express.static'('public'))
+app.use('/static', express.static('public'))
 
-app.use("/cats" , catsRoute)
+app.use('/cats', catsRoute)
 
 app.listen(PORT || 8000, () => console.log(`Listening on ${PORT}`))
