346d3a3bb96cc354cffef0d855e85853d7a9e038
JeffOrdze <jeff.ordze@live.ca>
2023-06-02 11:47:20 -0600

    get colors mapping to kitty cards

--- client/src/Components/KittyCard/KittyCard.js
+++ client/src/Components/KittyCard/KittyCard.js
@@ -3,53 +3,56 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 const KittyCard = () => {
-    const [ cats, setCats ] = useState(null);
+  const [cats, setCats] = useState(null);
 
-    useEffect( () => {
-        const getAKitty = async () => {
-            try {
-                const responseKitty = await axios.get("http://localhost:8080/cats")
-                const kitties = responseKitty.data;
-                setCats(kitties);
-                
-            }
-            catch(error) {
-                console.log(error)
-            }
-        }
-        getAKitty();    
+  useEffect(() => {
+    const getAKitty = async () => {
+      try {
+        const responseKitty = await axios.get("http://localhost:8080/cats");
+        const kitties = responseKitty.data;
+        setCats(kitties);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getAKitty();
+  }, []);
 
-    }, [])
+  if (!cats) {
+    return <h1>Loading...</h1>;
+  }
 
-    if(!cats) {
-        return(
-            <h1>Loading...</h1>
-    )}
-        
-    return (
-        <div>
-        {cats.map(cat => {
-            return(
-            <div className="kitty-card" key={cat.id}>
-            <img src={cat.img} alt="cat" className="kitty-card__img"/>
+  return (
+    <div>
+      {cats.map((cat, index) => {
+        const colors = ["#C16DF4", "#FA9C46", "#54BC5E", "#46A3FA"];
+        const backgroundColors = colors[index];
+        const cardStyle = { backgroundColor: backgroundColors };
+
+        return (
+          <div className="kitty-card" key={cat.id}>
+            <img src={cat.img} alt="cat" className="kitty-card__img" />
             <div className="kitty-card__copy">
-                <div className="kitty-card__name">{cat.name}</div>
-                <div className="kitty-card__card-body">
-                    <p className="kitty-card__description paragraph">
-                    {cat.description}
-                    </p>
-                    <div className="kitty-card__bottom-div">
-                        <p className="kitty-card__price">Price: ${cat.price}/day</p>
-                        <button className="kitty-card__button button">Add to Box</button>
-                    </div>
+              <div className="kitty-card__name" style={cardStyle}>
+                {cat.name}
+              </div>
+              <div className="kitty-card__card-body">
+                <p className="kitty-card__description paragraph">
+                  {cat.description}
+                </p>
+                <div className="kitty-card__bottom-div">
+                  <p className="kitty-card__price">Price: ${cat.price}/day</p>
+                  <button className="kitty-card__button button" style={cardStyle}>
+                    Add to Box
+                  </button>
                 </div>
+              </div>
             </div>
-        </div>
-        )
-        })
-    }
+          </div>
+        );
+      })}
     </div>
-    ) 
-}
+  );
+};
 
 export default KittyCard;
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "@fontsource/roboto"
+import "./styles/partials/global.scss";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
