ce1cd13ebfe130622f004d5fcfe703f7808cacbd
JeffOrdze <jeff.ordze@live.ca>
2023-06-02 10:11:08 -0600

    add logic for single cat request

--- server/routes/cats.js
+++ server/routes/cats.js
@@ -2,12 +2,18 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 
+router.get("/", (_req, res) => {
+  const catData = JSON.parse(fs.readFileSync("./data/cats.json"));
 
-router.get('/', (_req,res) => {
-const catData = JSON.parse(fs.readFileSync('./data/cats.json'))
+  res.status(200).send(catData);
+});
 
-res.status(200).send(catData)
-}
-)
+router.get("/checkout/:id", (req,res) => { 
+    const catId = req.params.id
+    const catData = JSON.parse(fs.readFileSync('./data/cats.json'));
+    const selectedCat = catData.filter(cat => cat.id === catId)
+
+    res.status(200).send(selectedCat)
+})
 
 module.exports = router;
