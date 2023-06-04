79c3dfbf878de19dce40bd1ecaa53019edd87e6b
KatKowalik <katarzyna.kowalik2@gmail.com>
2023-06-04 19:12:59 -0400

    added /bin/zsh when the box is empty

--- client/src/Components/MyBox/MyBox.js
+++ client/src/Components/MyBox/MyBox.js
@@ -3,15 +3,21 @@ import BoxItem from "../BoxItem/BoxItem";
 import { Link } from 'react-router-dom'
 
 const MyBox = ({state}) => {
+    
     return (
         <div className="my-box">
             <h2 className="my-box__title sub-header">My Box</h2>
            {state.data !== undefined ? <BoxItem state={state}/> : null}
             <div className="my-box__total-div">
                 <p className="my-box__total body-copy">Total:</p>
-                <p className="my-box__price body-copy">$150</p>
+                {
+                    state.data?
+                    (<p className="my-box__price body-copy">${state.data[0].price}</p>)
+                    :
+                    (<p className="my-box__price body-copy">$0</p>)
+                }
             </div>
-           <Link to="/confirmation"> 
+           <Link to="/confirmation" className="my-box__button-link"> 
            <button className="my-box__button button-large btnLarge">Checkout</button>
            </Link> 
         </div>
